c6ae6e4a140d5cae792df5e13fdfa03f483056d4
lizhengpeng666 <2818508206@qq.com>
2025-07-17 20:50:26 +0800

    完成获取当前用户的接口以及加入活动的接口

--- code/front/pq-vite/src/main.js
+++ code/front/pq-vite/src/main.js
@@ -1,16 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import Antd from 'ant-design-vue';
+//import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router'
+import { createPinia } from 'pinia'
 
 const app = createApp(App)
 
 
 app.use(router)
+app.use(createPinia())
 app.use(ElementPlus)
 app.mount('#app')
 
---
+++ code/front/pq-vite/src/stores/userInfo.js
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+
+export const useUserInfoStore = defineStore('user', {
+  state: () => ({
+    id: 0,
+    username: '测试名',
+    nickname: '孙悟空'
+  }),
+  actions: {
+    setUserInfo({ id, username, nickname}) {
+      this.id = id
+      this.username = username
+      this.nickname = nickname
+    },
+    clearUserInfo() {
+      this.id = null
+      this.username = ''
+      this.nickname = ''
+    }
+  }
+})
